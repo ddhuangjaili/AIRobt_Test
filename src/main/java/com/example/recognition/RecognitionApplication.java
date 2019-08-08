0df1bf113d162b67262683d4d8179c8d19ae59ce
perry <340090230@qq.com>
2019-08-09 02:25:17 +0800

    feat(ai): ocr路由

--- src/main/java/com/example/recognition/RecognitionApplication.java
+++ src/main/java/com/example/recognition/RecognitionApplication.java
@@ -1,12 +1,9 @@
 package com.example.recognition;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@MapperScan("com.example.**.mapper")
-@SpringBootApplication/*(exclude = DataSourceAutoConfiguration.class)*/
+@SpringBootApplication
 public class RecognitionApplication {
 
 	public static void main(String[] args) {
